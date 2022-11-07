4d609655ee52ec6723bf70c0c0e6cdd7888ba3e8
Harshavardhan-Suresh <66552332+Harshavardhan-Suresh@users.noreply.github.com>
2022-11-07 07:40:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { Header } from "./components/Header";
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { UserBlogs } from "./components/UserBlogs";
+import Auth from "./components/Auth";
+import { AddBlog } from "./components/AddBlog";
+import { BlogDetail } from "./components/BlogDetail";
+import Blogs from "./components/Blogs";
+import Profile from "./components/Profile";
+import UserProfile from "./components/UserProfile";
+import Followers from "./components/Followers";
+import Following from "./components/Following";
+function App() {
+  const [value, setValue] = useState();
+  const [refreshPage, setRefreshPage] = useState(0);
+  return <React.Fragment>
+    <header>
+      <Header value = {value} setValue={setValue} refreshPage={refreshPage} setRefreshPage={setRefreshPage}/>
+    </header>
+    <main >
+      <Routes >
+        <Route path="/auth" element={<Auth setValue={setValue} refreshPage={refreshPage} setRefreshPage={setRefreshPage}/>} />
+        <Route path="/blogs" element={<Blogs setValue={setValue} refreshPage={refreshPage} setRefreshPage={setRefreshPage}/>} />
+        <Route path="/myBlogs" element={<UserBlogs setValue={setValue} refreshPage={refreshPage} setRefreshPage={setRefreshPage}/>} />
+        <Route path="/myBlogs/:id" element={<BlogDetail />} />
+        <Route path="/user/" element={<Profile setValue={setValue} refreshPage={refreshPage} setRefreshPage={setRefreshPage}/>} />
+        <Route path="/user/:id" element={<UserProfile setValue={setValue} refreshPage={refreshPage} setRefreshPage={setRefreshPage}/>} />
+        <Route path="/following/:id" element={<Following setValue={setValue}/>} />
+        <Route path="/followers/:id" element={<Followers setValue={setValue}/>} />
+        <Route path="/blogs/add" element={<AddBlog setValue={setValue}/>} />
+      </Routes>
+    </main>
+  </React.Fragment>
+}
+
+export default App;
---
+++ src/components/Blogs.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Blog from "./Blog";
+const Blogs = ({setValue, refreshPage, setRefreshPage}) => {
+  const [blogs, setBlogs] = useState();
+  const sendRequest = async () => {
+    const res = await axios
+      .get("https://harsha-blogapp.herokuapp.com/api/blog")
+      .catch((err) => console.log(err));
+    console.log(res.data);
+    const data = res.data;
+    console.log(data);
+    return data;
+  };
+  useEffect(() => {
+    setValue(0);
+    sendRequest().then((data) => setBlogs(data.blogs));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [refreshPage]);
+  return (
+    <div>
+      {blogs &&
+        blogs.map((blog) => (
+          <Blog refreshPage={refreshPage} setRefreshPage={setRefreshPage}
+            id={blog._id}
+            isUser={localStorage.getItem("userId") === blog.user._id}
+            title={blog.title}
+            description={blog.description}
+            image={blog.image}
+            userName={blog.user.name}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Blogs;
